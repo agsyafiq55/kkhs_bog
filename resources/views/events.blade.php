9995f99904ff29ff3d7725ac5c2c1a59e64bc279
agsyafiq55 <agsyafiq55@gmail.com>
2025-03-25 15:33:57 +0800

    Working CRUD for events module

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -17,25 +17,33 @@
 <x-layouts.app>
     <h1 class="text-3xl font-semibold text-center mb-8">Events</h1>
     
-    <!--Events Sections-->
-    <div class="mt-6">
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-                    @foreach($events as $event)
-                        <article class="group flex rounded-radius max-w-sm flex-col overflow-hidden border border-outline bg-surface-alt dark:border-outline-dark dark:bg-surface-dark-alt">
-                            <div class="h-44 md:h-64 overflow-hidden">
-                                <img src="{{ $event->image_url ?? 'http://velocityacademy.org/wp-content/uploads/2016/03/placeholder.jpg' }}" class="object-cover transition duration-700 ease-out group-hover:scale-105" alt="{{ $event->title }}" />
-                            </div>
-                            <div class="flex flex-col gap-4 p-6">
-                                <div class="inline-block items-end">
-                                    <flux:badge color="{{ $tagColors[$event->tag] ?? 'zinc' }}" class="inline">{{ $event->tag }}</flux:badge>
-                                </div>
-                                <h3 class="text-xl font-bold">{{ $event->title }}</h3>
-                                <p class="text-sm">{{ $event->description }}</p>
-                            </div>
-                        </article>
-                    @endforeach
+<!--Events Sections-->
+<div class="mt-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
+        @foreach($events as $event)
+            <article class="group flex rounded-radius max-w-sm flex-col overflow-hidden border border-outline bg-surface-alt dark:border-outline-dark dark:bg-surface-dark-alt">
+                <div class="h-44 md:h-64 overflow-hidden">
+                    @if($event->thumbnail)
+                        <img src="data:image/jpeg;base64,{{ base64_encode($event->thumbnail) }}" 
+                             class="object-cover w-full h-full transition duration-700 ease-out group-hover:scale-105" 
+                             alt="{{ $event->title }}" />
+                    @else
+                        <img src="http://velocityacademy.org/wp-content/uploads/2016/03/placeholder.jpg" 
+                             class="object-cover w-full h-full transition duration-700 ease-out group-hover:scale-105" 
+                             alt="{{ $event->title }}" />
+                    @endif
                 </div>
-            </div>
+                <div class="flex flex-col gap-4 p-6">
+                    <div class="inline-block items-end">
+                        <flux:badge color="{{ $tagColors[$event->tag] ?? 'zinc' }}" class="inline">{{ $event->tag }}</flux:badge>
+                    </div>
+                    <h3 class="text-xl font-bold">{{ $event->title }}</h3>
+                    <p class="text-sm">{{ $event->description }}</p>
+                </div>
+            </article>
+        @endforeach
+    </div>
+</div>
 
     <!-- No events available message -->
     @if($events->isEmpty())
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,92 +27,88 @@
 <x-layouts.app :title="__('Home')">
 
     <!-- Main Content -->
-    <div class="p-4">
-            <div x-data="{            
-                slides: [                
-                    {
-                        imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-1.webp',
-                        imgAlt: 'Vibrant abstract painting with swirling blue and light pink hues on a canvas.',                
-                    },                
-                    {                    
-                        imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-2.webp',                    
-                        imgAlt: 'Vibrant abstract painting with swirling red, yellow, and pink hues on a canvas.',                
-                    },                
-                    {                    
-                        imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-3.webp',                    
-                        imgAlt: 'Vibrant abstract painting with swirling blue and purple hues on a canvas.',                
-                    },            
-                ],            
-                currentSlideIndex: 1,
-                touchStartX: null,
-                touchEndX: null,
-                swipeThreshold: 50,
-                previous() {                
-                    if (this.currentSlideIndex > 1) {                    
-                        this.currentSlideIndex = this.currentSlideIndex - 1                
-                    } else {   
-                        // If it's the first slide, go to the last slide           
-                        this.currentSlideIndex = this.slides.length                
-                    }            
-                },            
-                next() {                
-                    if (this.currentSlideIndex < this.slides.length) {                    
-                        this.currentSlideIndex = this.currentSlideIndex + 1                
-                    } else {                 
-                        // If it's the last slide, go to the first slide    
-                        this.currentSlideIndex = 1                
-                    }            
-                },        
-                handleTouchStart(event) {
-                    this.touchStartX = event.touches[0].clientX
-                },
-                handleTouchMove(event) {
-                    this.touchEndX = event.touches[0].clientX
-                },
-                handleTouchEnd() {
-                    if(this.touchEndX){
-                        if (this.touchStartX - this.touchEndX > this.swipeThreshold) {
-                            this.next()
-                        }
-                        if (this.touchStartX - this.touchEndX < -this.swipeThreshold) {
-                            this.previous()
-                        }
-                        this.touchStartX = null
-                        this.touchEndX = null
-                    }
-                },     
-            }" class="relative w-full max-w-full overflow-hidden">
+    <div>
+        <div x-data="{            
+        // Sets the time between each slides in milliseconds
+        autoplayIntervalTime: 4500,
+        slides: [                
+            {
+                imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-1.webp',
+                imgAlt: 'Vibrant abstract painting with swirling blue and light pink hues on a canvas.',  
+                title: 'Slide 1',
+                description: 'The architects of the digital world, constantly battling against their mortal enemy – browser compatibility.',           
+            },                
+            {                    
+                imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-2.webp',                    
+                imgAlt: 'Vibrant abstract painting with swirling red, yellow, and pink hues on a canvas.',  
+                title: 'Slide 2',
+                description: 'Because not all superheroes wear capes, some wear headphones and stare at terminal screens',            
+            },                
+            {                    
+                imgSrc: 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-3.webp',                    
+                imgAlt: 'Vibrant abstract painting with swirling blue and purple hues on a canvas.',    
+                title: 'Slide 3',
+                description: 'Where &quot;burnout&quot; is just a fancy term for &quot;Tuesday&quot;.',       
+            },            
+        ],            
+        currentSlideIndex: 1,
+        isPaused: false,
+        autoplayInterval: null,
+        previous() {                
+            if (this.currentSlideIndex > 1) {                    
+                this.currentSlideIndex = this.currentSlideIndex - 1                
+            } else {   
+                // If it's the first slide, go to the last slide           
+                this.currentSlideIndex = this.slides.length                
+            }            
+        },            
+        next() {                
+            if (this.currentSlideIndex < this.slides.length) {                    
+                this.currentSlideIndex = this.currentSlideIndex + 1                
+            } else {                 
+                // If it's the last slide, go to the first slide    
+                this.currentSlideIndex = 1                
+            }            
+        },    
+        autoplay() {
+            this.autoplayInterval = setInterval(() => {
+                if (! this.isPaused) {
+                    this.next()
+                }
+            }, this.autoplayIntervalTime)
+        },
+        // Updates interval time   
+        setAutoplayInterval(newIntervalTime) {
+            clearInterval(this.autoplayInterval)
+            this.autoplayIntervalTime = newIntervalTime
+            this.autoplay()
+        },    
+    }" x-init="autoplay" class="relative w-full overflow-hidden">
+    
+        <!-- slides -->
+        <!-- Change min-h-[50svh] to your preferred height size -->
+        <div class="relative min-h-[50svh] w-full">
+            <template x-for="(slide, index) in slides">
+                <div x-cloak x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.1000ms>
+                    
+                    <!-- Title and description -->
+                    <div class="lg:px-32 lg:py-14 absolute inset-0 z-10 flex flex-col items-center justify-end gap-2 bg-linear-to-t from-surface-dark/85 to-transparent px-16 py-12 text-center">
+                        <h3 class="w-full lg:w-[80%] text-balance text-2xl lg:text-3xl font-bold text-on-surface-dark-strong" x-text="slide.title" x-bind:aria-describedby="'slide' + (index + 1) + 'Description'"></h3>
+                        <p class="lg:w-1/2 w-full text-pretty text-sm text-on-surface-dark" x-text="slide.description" x-bind:id="'slide' + (index + 1) + 'Description'"></p>
+                    </div>
 
-                <!-- previous button -->
-                <button type="button" class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="previous slide" x-on:click="previous()">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pr-0.5" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                    </svg>
-                </button>
-
-                <!-- next button -->
-                <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-surface/40 p-2 text-on-surface transition hover:bg-surface/60 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:outline-offset-0 dark:bg-surface-dark/40 dark:text-on-surface-dark dark:hover:bg-surface-dark/60 dark:focus-visible:outline-primary-dark" aria-label="next slide" x-on:click="next()">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pl-0.5" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                    </svg>
-                </button>
-            
-                <!-- slides -->
-                <!-- Change min-h-[50svh] to your preferred height size -->
-                <div class="relative min-h-[50svh] w-full" x-on:touchstart="handleTouchStart($event)" x-on:touchmove="handleTouchMove($event)" x-on:touchend="handleTouchEnd()">
-                    <template x-for="(slide, index) in slides">
-                        <div x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.700ms>
-                            <img class="absolute w-full h-full inset-0 object-cover text-on-surface dark:text-on-surface-dark" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
-                        </div>
-                    </template>
+                    <img class="absolute w-full h-full inset-0 object-cover text-on-surface dark:text-on-surface-dark" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
                 </div>
-                
-                <!-- indicators -->
-                <div class="absolute rounded-radius bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 bg-surface/75 px-1.5 py-1 md:px-2 dark:bg-surface-dark/75" role="group" aria-label="slides" >
-                    <template x-for="(slide, index) in slides">
-                        <button class="size-2 rounded-full transition bg-on-surface dark:bg-on-surface-dark" x-on:click="currentSlideIndex = index + 1" x-bind:class="[currentSlideIndex === index + 1 ? 'bg-on-surface dark:bg-on-surface-dark' : 'bg-on-surface/50 dark:bg-on-surface-dark/50']" x-bind:aria-label="'slide ' + (index + 1)"></button>
-                    </template>
-                </div>
+            </template>
+        </div>
+        
+        <!-- indicators -->
+        <div class="absolute rounded-radius bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 px-1.5 py-1 md:px-2" role="group" aria-label="slides" >
+            <template x-for="(slide, index) in slides">
+                <button class="size-2 rounded-full transition" x-on:click="(currentSlideIndex = index + 1), setAutoplayInterval(autoplayIntervalTime)" x-bind:class="[currentSlideIndex === index + 1 ? 'bg-on-surface-dark' : 'bg-on-surface-dark/50']" x-bind:aria-label="'slide ' + (index + 1)"></button>
+            </template>
+        </div>
+    </div>
             </div>
 
             <!--Events Sections-->
@@ -123,7 +119,15 @@
                     @foreach($events as $event)
                         <article class="group flex rounded-radius max-w-sm flex-col overflow-hidden border border-outline bg-surface-alt dark:border-outline-dark dark:bg-surface-dark-alt">
                             <div class="h-44 md:h-64 overflow-hidden">
-                                <img src="{{ $event->image_url ?? 'http://velocityacademy.org/wp-content/uploads/2016/03/placeholder.jpg' }}" class="object-cover transition duration-700 ease-out group-hover:scale-105" alt="{{ $event->title }}" />
+                                @if($event->thumbnail)
+                                    <img src="data:image/jpeg;base64,{{ base64_encode($event->thumbnail) }}" 
+                                        class="object-cover w-full h-full transition duration-700 ease-out group-hover:scale-105" 
+                                        alt="{{ $event->title }}" />
+                                @else
+                                    <img src="http://velocityacademy.org/wp-content/uploads/2016/03/placeholder.jpg" 
+                                        class="object-cover w-full h-full transition duration-700 ease-out group-hover:scale-105" 
+                                        alt="{{ $event->title }}" />
+                                @endif
                             </div>
                             <div class="flex flex-col gap-4 p-6">
                                 <div class="inline-block items-end">
